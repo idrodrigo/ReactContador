5ca84c5f6eba0ce798939b137476c3c7866f4efd
Rho <117315116+idrodrigo@users.noreply.github.com>
2023-06-18 02:54:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setcontador(--contador)}>disminuir</button>
-      <button onClick={() => setcontador(contador+1)}>aumentar</button>
+      <button onClick={() => setcontador( prevState => -- prevState )}>disminuir</button>
+      <button onClick={() => setcontador( prevState => prevstate + 1 )}>aumentar</button>
       <button onClick={() => setcontador(0)}>restablecer</button>
       <h1>{contador}</h1>
 
